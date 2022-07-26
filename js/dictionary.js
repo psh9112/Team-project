a99f62eeb775faaf2428172f9425d5c8ae2a6359
an-saemi <ryogm97@gmail.com>
2022-07-26 10:45:58 +0900

    dictionary.js

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,4 +1,3 @@
-$('header > .all > .top > .logo > p').text("생활 쏙 법 용어");
 
 $.ajax({
     url: './api/worddictionary.json',
@@ -7,48 +6,42 @@ $.ajax({
 
         let list = [];
         let open = '';
-        let more = [];
-        let result= '';
+        let result = '';
+        let count = 0;
 
         $.each(word.data, function (key, value) {
-            list.push(`<li>
-                    <div class="word">
-                        <p>${value.용어명}</p>
-                        <span>+</span>
-                    </div>
-                    <div class="ex">
-                        <p>${value.설명}</p>
-                    </div>
-                </li>`);
-        })
-        open = list.slice(0, 5);
-        // console.log(open);
-        $('.wl > ul').html(open);
-        
 
+            if (key % 5 == 0 && key) {
+                list.push(result);
+                result = '';
+            }
+            result += `<li>
+                        <div class="word">
+                            <p>${value.용어명}</p>
+                            <span>+</span>
+                        </div>
+                        <div class="ex">
+                            <p>${value.설명}</p>
+                        </div>
+                    </li>`;
+        })
+
+        $('.wl > ul').html(list[0]);
+
+        $('li > .word').on('click', function () {
+            $('li > .ex').removeClass('plus');
+            $(this).next().addClass('plus');
+        })
 
         $('.wl > .more').on('click', function () {
-            for (let i = 6; i < list.length; i += 5) {
-                $.each(list,function(i,v){
-                    if(more.indexOf(v) == -1){
-                        more = list.slice(i, i + 5);
-                    };
-                });
-            };
-            open = open.concat(more);
-            $('.wl > ul').html(open);
-            console.log(open);
-        })
-        //더보기할때마다 배열값 추가되도록 하기
-        //반복되는 것만 빼면 됨 아즈아
+            count++;
+            $('.wl > ul').append(list[count]);
 
-
-        $('li > .word > span').on('click', function () {
-
-            $('li > .ex').removeClass('plus');
-            $(this).parent().next().addClass('plus');
-            //this의 부모의 형제요소인 '.ex'를 선택해서 plus라는 class값 추가함.
-
+            $('li > .word').on('click', function () {
+                console.log(0);
+                $('li > .ex').removeClass('plus');
+                $(this).next().addClass('plus');
+            })
         })
     }
 })
