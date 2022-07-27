9b6d7d5d3b1738723517693beb0d2c429f7391dd
psh9112 <106298368+psh9112@users.noreply.github.com>
2022-07-27 14:28:51 +0900

    re

--- js/common.js
+++ js/common.js
@@ -1,38 +1,39 @@
+
 $('header').load('inc.html > header > .all', hello);
 $('.popup').load('inc.html > .popup > .pop_bg');
 $('footer').load('inc.html > footer > .footer');
 
 
-function hello() {
-    $('.headline').css('padding-top', '5px');
+    function hello() {
+        $('.headline').css('padding-top', '5px');
 
-    let scrolltop = '';
-
-    $(document).on('scroll', function () {
-        scrollTop = $(document).scrollY;
-
-        $('header').addClass('fixedH');
-
-        // $('.tab').css('padding-top', '130px');
+        let scrolltop = '';
         
-        if (scrollTop <= 100) {
-            $('header').removeClass('fixedH');
-            $('.tab').css('padding-top', '0px');
-        }
-    })
+        $(document).on('scroll', function () {
+            scrollTop = $(document).scrollY;
 
-    $('.burger').on('click', function () {
-        $('.popup').addClass('active');
-        $('.pop_bg').addClass('left');
-        // setAttribute는 나중에
+            $('header').addClass('fixedH');
 
-        $('.right').on('click', function () {
-            $('.popup').removeClass('active');
-            $('.pop_bg').css('transform', 'translateX(100%)');
+            $('.tab').css('padding-top', '130px');
+
+            if (scrollTop <= 100) {
+                $('header').removeClass('fixedH');
+                $('.tab').css('padding-top', '0px');
+            }
         })
 
+        $('.burger').on('click', function () {
+            $('.popup').addClass('active');
+            $('.pop_bg').addClass('left');
+            // setAttribute는 나중에
+
+            $('.right').on('click', function () {
+                $('.popup').removeClass('active');
+                $('.pop_bg').css('transform', 'translateX(100%)');
+            })
 
 
-    })
-}
+
+        })
+    }
 
