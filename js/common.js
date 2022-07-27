b87bcae515052e9f544d7959a4d7e850282572e2
an-saemi <ryogm97@gmail.com>
2022-07-27 15:04:37 +0900

    공통헤더 fixed

--- js/common.js
+++ js/common.js
@@ -1,22 +1,28 @@
-$('header').load('inc.html > header > .all',hello);
+$('header').load('inc.html > header > .all', hello);
 $('.popup').load('inc.html > .popup > .pop_bg');
 $('footer').load('inc.html > footer > .footer');
 
 
 function hello() {
+    let scrolltop = '';
 
-    $('.burger').on('click', function () {
-        $('.popup').addClass('active');
-        $('.pop_bg').addClass('left');
-        // setAttribute는 나중에
+    $(document).on('scroll', function () {
+        scrolltop = $(document).scrollY;
 
-        $('.right').on('click', function () {
-            $('.popup').removeClass('active');
-            $('.pop_bg').css('transform', 'translateX(100%)');
+        $('header').addClass('fixedH');
+        $('.tab').css('padding-top', '130px');
+
+        //버거popup 스크립트만
+        $('.burger').on('click', function () {
+            $('.popup').addClass('active');
+            $('.pop_bg').addClass('left');
+            // setAttribute는 나중에
+
+            $('.right').on('click', function () {
+                $('.popup').removeClass('active');
+                $('.pop_bg').css('transform', 'translateX(100%)');
+            })
+
         })
-
-
-
     })
 }
-
