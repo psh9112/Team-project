7f03f81cf71006a6bd2bf3007512ba869aa9fb11
psh9112 <106298368+psh9112@users.noreply.github.com>
2022-09-13 13:40:54 +0900

    re

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,27 +1,14 @@
-let aaa = '';
-$('.popup').ready(function () {
-    setTimeout(function () {
-        $('#ipt').bind('input', function () {
-            // localStorage.input = ipt.value();
-            aaa = $('#ipt').val();
-
-            localStorage.setItem("key", aaa);
-
-            console.log(aaa);
-        });
-
-    }, 500, clearTimeout)
-});
-
 $.ajax({
     url: './api/worddictionary.json',
     success: function (word) {
         console.log('loading_fin');
 
         let list = [];
-        let open = '';
         let result = '';
         let count = 0;
+
+        let option = '';
+
 
         $.each(word.data, function (key, value) {
 
@@ -43,37 +30,104 @@ $.ajax({
                             <p>${value.설명}</p>
                         </div>
                     </li>`;
-        })
 
-        $('.wl > ul').html(list[0]);
+            $('.wl > ul').html(list[0]);
+        });
+
 
         $('li > .word').on('click', function () {
-            $('li > .ex').removeClass('plus');
-            $(this).next().toggleClass('plus');
+            $('li').find('button').removeClass('active');
             $(this).find('button').toggleClass('active');
+            
+            $('li > .answer').removeClass('plus');
+            $(this).next().toggleClass('plus');
         })
 
         $('.wl > .more').on('click', function () {
             count++;
-            
+
             $('.wl > ul').append(list[count]);
 
             $('li > .word').on('click', function () {
-                $('li > .ex').removeClass('plus');
-                $(this).next().toggleClass('plus');
+                $('li').find('button').removeClass('active');
                 $(this).find('button').toggleClass('active');
+
+                $('li > .answer').removeClass('plus');
+                $(this).next().toggleClass('plus');
             })
         })
 
 
+        // 검색기능
         $('.search p button').on('click', function () {
-            if (list.val() == $('.search #word').val()) {
-                $('.wl > ul').html(list);
-            } s
-            console.log($('.wl > ul').html(list[key]));
-        });
+            count = 0;
+            option = $('#search').val();
+            console.log(option);
+
+            print(option);
+
+
+            $('li > .word').on('click', function () {
+                $(this).find('button').toggleClass('active');
+                $(this).next().toggleClass('plus');
+
+                $('li').find('button').removeClass('active');
+                $('li > .answer').removeClass('plus');
+            })
+
+        }
+
+        );
+
+        // ----------------------------------------------------------
+        function print(param) {
+            //data print 
+            let elData = [], elList = [], elPush = '';
+
+            word.data.forEach(function (v) {
+                try {
+                    if (v.용어명.includes(param)) {
+                        elData.push(`<li>
+                        <div class="word">
+                            <p>${v.용어명}</p>
+                            <div class="du">
+                                <button> 
+                                    <span></span>
+                                    <span></span>
+                                <button>
+                            </div>
+                        </div>
+                        <div class="answer">
+                            <p>${v.설명}</p>
+                        </div>
+                    </li>`);
+                    }
+                } catch { }
+
+            })
+
+            elData.forEach(function (t, k) {
+                if (k % 6 == 0 && k) {
+                    elList.push(elPush);
+                    elPush = '';
+                }
+                elPush += t;
+            });
+
+
+            $('.wl > ul').html(elList);
+
+            if (!option == elList) {
+                $('.wl > ul').html('<li>검색결과 일치한 값이 없습니다</li>');
+            }
+        }
+
     }
 })
+
+
+
+
 
 
 
